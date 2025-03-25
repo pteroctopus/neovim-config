c611140dd6d5c6281d3500479dff3a674628a587
pteroctopus <gnovak.eml@gmail.com>
2025-03-25 14:13:02 +0100

    CopilotChat plugin update

--- lua/language-servers.lua
+++ lua/language-servers.lua
@@ -55,17 +55,20 @@ local servers = {
   yamlls = {
     yaml = {
       schemas = {
+        -- ["file:///Users/goran.novak/Development/github/kubeconform/scripts/crdtest.json"] = "*.x.yaml",
         -- helm unittest
         ["https://raw.githubusercontent.com/helm-unittest/helm-unittest/v0.4.1/schema/helm-testsuite.json"] = "tests/*_test.yaml",
         ["http://json.schemastore.org/kustomization"] = "kustomization.{yml,yaml}",
         ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
         ["https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/schemas/json/helmfile.json"] = "helmfile.{yml,yaml}",
         kubernetes = {
+          -- '*.yaml',
+          --
           '*.kube.yml',
           '*.kube.yaml',
           '*kubectl-edit-*.yaml',
           '*role*/templates/create*.y*ml',
-        }
+        },
       },
       schemaStore = {
         enable = true,
