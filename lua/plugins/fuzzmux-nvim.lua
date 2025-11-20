99f2e26219924c076a72947dcca9667e20a501cb
pteroctopus <gnovak.eml@gmail.com>
2025-11-20 12:21:47 +0100

    Fuzzmux plugin added

--- lua/plugins/fuzzmux-nvim.lua
+++ lua/plugins/fuzzmux-nvim.lua
@@ -1,10 +1,12 @@
 return {
+
   'pteroctopus/fuzzmux.nvim',
   opts = {
     -- Optional configuration
     -- Currently no options available
     -- This is a placeholder for future configuration
   },
+
   -- enabled = true,
   -- dir = "~/Development/github/fuzzmux.nvim/",
   -- dev = true,
