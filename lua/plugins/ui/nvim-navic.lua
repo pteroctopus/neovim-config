2a1159107e2d53c4a9a888219de6ebb1249951e6
pteroctopus <gnovak.eml@gmail.com>
2026-02-14 13:11:27 +0100

    Navic now works without workaround

--- lua/plugins/ui/nvim-navic.lua
+++ lua/plugins/ui/nvim-navic.lua
@@ -1,17 +1,3 @@
--- TODO: This is a workaround (see how to do it correctly)!! because of nvim-navic plugin.
--- It needs language server support to generate breadcrumbs and ansiblels doesn't have it.
--- This autocommand will change the filetype of yaml.ansible file to yaml so that yamlls language server
--- starts also and switch back filetype to yaml.ansible
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.name == "ansiblels" then
-      vim.bo[args.buf].filetype = "yaml"
-      vim.bo[args.buf].filetype = "yaml.ansible"
-    end
-  end,
-})
-
 -- Plugin configuration
 return {
   "SmiteshP/nvim-navic",
