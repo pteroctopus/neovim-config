9658ac71492b50bd8d66781a7d449fbadb76d7b4
pteroctopus <gnovak.eml@gmail.com>
2026-03-10 13:08:58 +0100

    lazydev workaround

--- lua/plugins/language_support/lazydev.lua
+++ lua/plugins/language_support/lazydev.lua
@@ -1,8 +1,10 @@
+-- doesn't load every time if lazy is true and if we remove priority
+-- need to see why this is happening
+-- for now workaround with priority works
 return {
   {
     "folke/lazydev.nvim",
-    lazy = true,
-    ft = "lua",
+    priority = 1001,
     opts = {
       library = {
         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
