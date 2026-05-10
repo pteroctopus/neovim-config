a27d57642d82166eae9159177acc22bc93d277b5
pteroctopus <gnovak.eml@gmail.com>
2026-05-10 08:46:14 +0200

    Updated faster.nvim with fix for recursive macro

--- lua/plugins/faster-nvim.lua
+++ lua/plugins/faster-nvim.lua
@@ -1,7 +1,6 @@
 return {
   "pteroctopus/faster.nvim",
   branch = "fix/recursive-macro-fail",
-  -- enabled = false,
   -- dir = "~/Development/github/faster.nvim/",
   -- dev = true,
   -- name = "faster.nvim",
